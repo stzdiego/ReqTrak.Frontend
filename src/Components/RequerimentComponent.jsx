82a79411fa38f67b708b9c132ecc70b86dc082a1
Diego Armando Santacruz <diego.santacruz@siesa.com>
2024-11-11 22:28:33 -0500

    Mejoras en Requeriments

--- src/Components/RequerimentComponent.jsx
+++ src/Components/RequerimentComponent.jsx
@@ -1,55 +1,72 @@
 import React from "react";
 import styles from "./RequerimentComponent.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt, faUser, faCalendarAlt, faFlag, faUserTag } from '@fortawesome/free-solid-svg-icons';
 
-const RequerimentComponent = ({ id, creationDate, userCreator, subject, description, stage, priority, userAssigned }) => {
+const priorityMap = {
+    '-1': 'None',
+    '0': 'Low',
+    '1': 'Medium',
+    '2': 'High'
+};
+
+const stageMap = {
+    '-1': 'None',
+    '0': 'Pending',
+    '1': 'Rejected',
+    '2': 'Approved',
+    '3': 'Planned',
+    '4': 'InProgress',
+    '6': 'Done'
+};
+
+const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+const RequerimentComponent = ({ id, createdAt, userCreator, subject, description, stage, priority, userAssigned }) => {
+    const handleDetailClick = () => {
+        window.open(`/requeriment/${id}`, '_blank');
+    };
+
     return (
         <div className={`card ${styles.cardReq}`}>
-            <div className={styles.verticalLine}></div>
+            <div className={styles.verticalContainer}>
+                <button className={`btn btn-outline-primary ${styles.detailButton}`} onClick={handleDetailClick}>
+                    <FontAwesomeIcon icon={faExternalLinkAlt} />
+                </button>
+                <div className={styles.verticalSeparator}></div>
+            </div>
             <div className={styles.content}>
                 <div className={styles.row}>
                     <div className="input-group me-3 mb-1">
-                        <span className="input-group-text">Id</span>
-                        <input type="text" className="form-control" placeholder="Id" aria-label="Id" value={id}
-                               readOnly="true"/>
+                        <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
+                        <input type="text" className="form-control" placeholder="Creator" aria-label="Creator" value={userCreator ? userCreator.name : ''} readOnly />
                     </div>
                     <div className="input-group me-3 mb-1">
-                        <span className="input-group-text">Creator</span>
-                        <input type="text" className="form-control" placeholder="Id" aria-label="Id" value={userCreator}
-                               readOnly="true"/>
-                    </div>
-                    <div className="input-group me-3 mb-1">
-                        <span className="input-group-text">Date</span>
-                        <input type="text" className="form-control" placeholder="Id" aria-label="Id" value={creationDate}
-                               readOnly="true"/>
+                        <span className="input-group-text"><FontAwesomeIcon icon={faCalendarAlt} /></span>
+                        <input type="text" className="form-control" placeholder="Date" aria-label="Date" value={formatDate(createdAt)} readOnly />
                     </div>
                 </div>
 
                 <div className="form-floating me-3 mb-1">
-                    <input className="form-control" placeholder="Subject" readOnly="true" value={subject}></input>
+                    <input className="form-control" placeholder="Subject" readOnly value={subject}></input>
                     <label>Subject</label>
-                </div>
-
-                <div className="form-floating me-3 mb-1">
-                    <textarea className="form-control" placeholder="Subject" readOnly="true"
-                              value={description}></textarea>
-                    <label>Description</label>
                 </div>
 
                 <div className={styles.row}>
                     <div className="input-group me-3 mb-1">
-                        <span className="input-group-text">Stage</span>
-                        <input type="text" className="form-control" placeholder="Id" aria-label="Id" value={stage}
-                               readOnly="true" />
+                        <span className="input-group-text"><FontAwesomeIcon icon={faFlag} /></span>
+                        <input type="text" className="form-control" placeholder="Stage" aria-label="Stage" value={stageMap[stage]} readOnly />
                     </div>
                     <div className="input-group me-3 mb-1">
-                        <span className="input-group-text">Priority</span>
-                        <input type="text" className="form-control" placeholder="Id" aria-label="Id" value={priority}
-                               readOnly="true"/>
+                        <span className="input-group-text"><FontAwesomeIcon icon={faFlag} /></span>
+                        <input type="text" className="form-control" placeholder="Priority" aria-label="Priority" value={priorityMap[priority]} readOnly />
                     </div>
                     <div className="input-group me-3 mb-1">
-                        <span className="input-group-text">Assigned</span>
-                        <input type="text" className="form-control" placeholder="Id" aria-label="Id" value={userAssigned}
-                               readOnly="true"/>
+                        <span className="input-group-text"><FontAwesomeIcon icon={faUserTag} /></span>
+                        <input type="text" className="form-control" placeholder="Assigned" aria-label="Assigned" value={userAssigned ? userAssigned.name : 'Unassigned'} readOnly />
                     </div>
                 </div>
             </div>
