5d8fd74bdd4d438016957f857184b50ca88779d6
Diego Armando Santacruz <diego.santacruz@siesa.com>
2024-11-12 23:25:05 -0500

    Mejoras para Netlify

--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 // Import admin-specific pages and components here
 
 const AdminRoutes = () => (
